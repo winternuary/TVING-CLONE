0dd541b95dd99d839143221690c3f3e93e9f8aa4
zeroeuni <pkhj286@gmail.com>
2024-06-07 16:26:23 +0900

    Add: 헤더

---
+++ src/components/header.jsx
@@ -0,0 +1,18 @@
+import * as S from "./style";
+import Logo from "../assets/Logo.svg";
+import Member from "../assets/button1.png";
+import Login from "../assets/button2.png";
+
+function Header() {
+  return (
+    <S.HeaderLayout>
+      <S.Logo src={Logo} />
+      <S.ButtonBox>
+        <S.Member src={Member} />
+        <S.Login src={Login} />
+      </S.ButtonBox>
+    </S.HeaderLayout>
+  );
+}
+
+export default Header;
---
+++ src/components/style.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const HeaderLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding: 1.5% 2%;
+`;
+
+export const Logo = styled.img`
+  width: 6%;
+`;
+
+export const ButtonBox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 5%;
+`;
+
+export const Member = styled.img`
+  display: flex;
+  padding: 4%;
+`;
+
+export const Login = styled.img`
+  display: flex;
+  padding: 4%;
+`;
